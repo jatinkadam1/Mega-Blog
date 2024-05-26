a6f1634c1a08e022ed74bae4cccde7ce2bc887c7
Jatin Kadam <jatinkadam2005@gmail.com>
2024-05-26 13:04:55 +0530

    Added redux

--- mega/src/App.jsx
+++ mega/src/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/auth'
+import { login, logout } from './store/authSlice' 
+import Header from "../src/components/Header"
+import Footer from "../src/components/Footer"
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h3>Hello</h3>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>hello
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ): null
 }
 
 export default App
